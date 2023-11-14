64c64c0281dff403382dbf4894a59a222800ae8a
vvvjh112 <lleell6@naver.com>
2023-11-14 14:43:43 +0900

    .

--- Problem.py
+++ Problem.py
@@ -66,13 +66,62 @@
 # print(int(answer))
 
 #작업형 1 - 5
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P210302.csv')
+# result = data.isna().sum()/len(data)
+# result = pd.DataFrame(result)
+# result = result.sort_values(by=0, ascending = False)
+# print(result.index[0])
 
-data = pd.read_csv('dataset/P210302.csv')
-result = []
-print(data.isna().sum())
-for i in data.columns:
-    leng = data[i].isna().sum()/len(data[i])
-    result.append(leng)
-print(result)
+#작업형 1 - 6
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P210303.csv')
+#
+# data = data[['country', 'year', 'new_sp']]
+# data = data.dropna()
+# data = data[data['year']==2000]
+# country_mean = round(data["new_sp"].mean(),2)
+# print(country_mean)
+# result = data[data["new_sp"]>=country_mean]
+# print(len(result))
+
+#작업형 1 - 7
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P220401.csv')
+#
+# y1 = np.percentile(data['y'],25)
+# y2 = np.percentile(data['y'],75)
+# result = int(y2-y1)
+# print(result)
+
+#작업형 1 - 8
+# import pandas as pd
+# import numpy as np
+# #좋아요 놀랐어요 긍정 으로 치고 비율 계산해서 0.4 ~ 0.5
+# data = pd.read_csv('dataset/P220402.csv')
+#
+# data['temp'] = (data['num_loves']+data['num_wows'])/data['num_reactions']
+#
+# result = data[data['temp']>0.4]
+# result = result[result['temp']<0.5]
+# result = result[result['status_type']=='video']
+# answer = len(result)
+# print(answer)
+
+#작업형 1 - 9
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P220501.csv')
+# print(data.columns)
+# data = data[data['종량제봉투용도']=="음식물쓰레기"]
+# data = data[data['종량제봉투종류']=="규격봉투"]
+# data = data[data['2L가격']>0]
+# answer = int(round(data['2L가격'].mean(),0))
+# print(answer)
