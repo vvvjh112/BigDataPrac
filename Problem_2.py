dc0128d584117869c2994299c04820cc3c95fd26
vvvjh112 <lleell6@naver.com>
2023-11-16 17:07:48 +0900

    .

--- Problem_2.py
+++ Problem_2.py
@@ -137,55 +137,160 @@
 # result.to_csv('Result/Problem2-2.csv',index=False)
 
 #2 - 3
+# import pandas as pd
+# import numpy as np
+#
+# train = pd.read_csv('dataset/P220404-01.csv')
+# test = pd.read_csv('dataset/P220404-02.csv')
+# test_id = test['ID']
+#
+# # 'ID', 'Gender', 'Ever_Married', 'Age', 'Graduated', 'Profession',
+# #        'Work_Experience', 'Spending_Score', 'Family_Size', 'Var_1',
+# #        'Segmentation'
+#
+# ary = ['Gender','Ever_Married','Graduated','Profession','Spending_Score','Var_1']
+# from sklearn.preprocessing import *
+# train['Segmentation'] = train['Segmentation'].astype('category')
+# for i in ary:
+#     train[i] = LabelEncoder().fit_transform(train[i])
+#     test[i] = LabelEncoder().fit_transform(test[i])
+#
+# print(train.info())
+#
+#
+# from sklearn.model_selection import *
+# from sklearn.ensemble import *
+# from sklearn.metrics import *
+#
+# train = train.drop(['ID'], axis =1 )
+# test = test.drop('ID',axis = 1)
+#
+# x= train.drop('Segmentation',axis =1)
+# y = train['Segmentation']
+#
+# trainX, testX, trainY, testY = train_test_split(x,y,test_size = 0.2)
+#
+# model = RandomForestClassifier(n_estimators = 600, max_depth = 6)
+#
+# model.fit(trainX,trainY)
+#
+# pred = model.predict(testX)
+#
+# print(pred)
+#
+#
+# print("f1score",f1_score(testY,pred,labels=['A','B','C','D'],average='macro'))
+#
+# model.fit(x,y)
+#
+# pred_result = model.predict(test)
+#
+#
+# result = pd.DataFrame({"ID":test_id, "pred" : pred_result})
+#
+# result.to_csv("Result/Problem_2-3.csv",index=False)
+
+#2 -4
+# import pandas as pd
+# import numpy as np
+#
+# train = pd.read_csv('dataset/P220504-01.csv')
+# test = pd.read_csv('dataset/P220504-02.csv')
+# # 'model', 'year', 'price', 'transmission', 'mileage', 'fuelType', 'tax', 'mpg', 'engineSize']
+#
+# from sklearn.preprocessing import *
+# from sklearn.metrics import *
+# from sklearn.model_selection import *
+# from sklearn.ensemble import *
+#
+# print(train.info())
+#
+# train['model'] = train['model'].astype('category')
+# test['model'] = test['model'].astype('category')
+# Label_lst = ['transmission','fuelType','model']
+# int_lst = ['mileage','tax']
+# def min_max(x):
+#     return (x-min(x))/(max(x)-min(x))
+#
+# for i in Label_lst:
+#     train[i] = LabelEncoder().fit_transform(train[i])
+#     test[i] = LabelEncoder().fit_transform(test[i])
+#
+# for i in int_lst:
+#     train[i] = min_max(train[i])
+#     test[i] = min_max(test[i])
+#
+# print(train.info())
+#
+# print(train.isna().sum())
+#
+# x = train.drop('price',axis = 1)
+# y = train['price']
+#
+# trainX, testX, trainY, testY = train_test_split(x,y,test_size=0.2)
+#
+# model = RandomForestRegressor(n_estimators = 600, max_depth = 4)
+#
+# model.fit(trainX,trainY)
+#
+# pred = model.predict(testX)
+#
+# print("Root Mean Squared Error", mean_squared_error(testY,pred,squared = False))
+#
+# model.fit(x,y)
+#
+# pred = model.predict(test)
+#
+# result = pd.DataFrame({"pred":pred})
+#
+# print(result)
+#
+# result.to_csv("Result/Problem2-4.csv",index = False)
+
+
+#2 - 5
 import pandas as pd
-import numpy as np
 
-train = pd.read_csv('dataset/P220404-01.csv')
-test = pd.read_csv('dataset/P220404-02.csv')
-test_id = test['ID']
+train = pd.read_csv('dataset/P230604-01.csv')
+test = pd.read_csv('dataset/P230604-02.csv')
 
-# 'ID', 'Gender', 'Ever_Married', 'Age', 'Graduated', 'Profession',
-#        'Work_Experience', 'Spending_Score', 'Family_Size', 'Var_1',
-#        'Segmentation'
-
-ary = ['Gender','Ever_Married','Graduated','Profession','Spending_Score','Var_1']
-from sklearn.preprocessing import *
-train['Segmentation'] = train['Segmentation'].astype('category')
-for i in ary:
-    train[i] = LabelEncoder().fit_transform(train[i])
-    test[i] = LabelEncoder().fit_transform(test[i])
-
+# 'battery_power', 'blue', 'clock_speed', 'dual_sim', 'fc', 'four_g',
+#        'int_memory', 'm_dep', 'mobile_wt', 'n_cores', 'pc', 'px_height',
+#        'px_width', 'ram', 'sc_h', 'sc_w', 'talk_time', 'three_g',
+#        'touch_screen', 'wifi', 'price_range'
+print(train.columns)
 print(train.info())
+# print(train.head())
 
+print(train.describe())
 
-from sklearn.model_selection import *
-from sklearn.ensemble import *
+category_lst = ['blue','touch_screen','wifi','dual_sim','four_g','three_g','n_cores']
+min_max_lst = ['battery_power','clock_speed','fc','int_memory','px_height','px_width']
+
 from sklearn.metrics import *
+from sklearn.model_selection import *
+from sklearn.preprocessing import *
+from sklearn.ensemble import *
+# print(train[['talk_time','three_g']].head())
+def min_max(x):
+    return (x-min(x))/(max(x)-min(x))
+train['painrice_range'] = train['price_range'].astype('category')
+for i in category_lst:
+    train[i] = train[i].astype('category')
+    test[i] = test[i].astype('category')
+x = train.drop('price_range',axis =1)
+y = train['price_range']
 
-train = train.drop(['ID'], axis =1 )
-test = test.drop('ID',axis = 1)
+x = pd.get_dummies(x)
 
-x= train.drop('Segmentation',axis =1)
-y = train['Segmentation']
+trainX, testX, trainY, testY = train_test_split(x,y,test_size=0.2)
 
-trainX, testX, trainY, testY = train_test_split(x,y,test_size = 0.2)
-
-model = RandomForestClassifier(n_estimators = 600, max_depth = 6)
+model = RandomForestClassifier(n_estimators = 600, max_depth = 5)
 
 model.fit(trainX,trainY)
 
 pred = model.predict(testX)
 
-print(pred)
+print('Accuracy',accuracy_score(testY,pred))
 
-
-print("f1score",f1_score(testY,pred,labels=['A','B','C','D'],average='macro'))
-
-model.fit(x,y)
-
-pred_result = model.predict(test)
-
-
-result = pd.DataFrame({"ID":test_id, "pred" : pred_result})
-
-result.to_csv("Result/Problem_2-3.csv",index=False)
+# print('macro_f1',f1_score(testY,pred, labels = [0,1,2,3],average = 'macro'))
