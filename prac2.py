ae9e7659c487bb7abe216cd7d47242d29b55f220
vvvjh112 <lleell6@naver.com>
2023-11-22 15:28:37 +0900

    .

--- prac2.py
+++ prac2.py
@@ -118,21 +118,86 @@
 # print(answer)
 
 #1-9
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P220403.csv')
+#
+# print(data['date_added'].head())
+#
+# data['date_added'] = pd.to_datetime(data['date_added'],format = '%B %d, %Y')
+#
+# print(data['date_added'].head())
+#
+# data = data[(data['date_added'].dt.year == 2018)&(data['date_added'].dt.month == 1)]
+#
+# data = data[data['country']=='United Kingdom']
+#
+# answer=(len(data))
+#
+# print(answer)
 
-data = pd.read_csv('dataset/P220403.csv')
+# 1-10
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P220501.csv')
+#
+# print(data.columns)
+#
+# data = data[(data['종량제봉투종류'] == '규격봉투') & (data['종량제봉투용도']=='음식물쓰레기')&(data['2L가격']>0)]
+#
+# result = data['2L가격']
+#
+# answer = int(round(result.mean()))
+# print(answer)
 
-print(data['date_added'].head())
+#1 -11
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P220502.csv')
+#
+# def bmi(w,h):
+#     return w/((h/100)**2)
+#
+# data['bmi'] = bmi(data['Weight'],data['Height'])
+# #정상 - 위험 절댓값
+# normal = len(data[(data['bmi']>=18.5) & (data['bmi']<23)])
+# warning = len(data[(data['bmi']>=23) & (data['bmi']<25)])
+#
+# answer = int(round(abs(normal-warning),0))
+# print(answer)
 
-data['date_added'] = pd.to_datetime(data['date_added'],format = '%B %d, %Y')
+#1-12
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P220503.csv')
+#
+# print(data.columns)
+#
+# data['sun'] = data['전입학생수(계)']-data['전출학생수(계)']
+#
+# result = data.sort_values('sun',ascending = False).head(1)
+#
+# answer = (result['전체학생수(계)'].iloc[0])
+#
+# print(answer)
 
-print(data['date_added'].head())
+#1-13
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/P230601.csv')
+#
+# data['신고일시'] = pd.to_datetime(data['신고일시'])
+# data['출동일시'] = pd.to_datetime(data['출동일시'])
+# data['소요시간'] = (data['출동일시']-data['신고일시']).dt.total_seconds()
 
-data = data[(data['date_added'].dt.year == 2018)&(data['date_added'].dt.month == 1)]
+# group = data.groupby([data['출동소방서'],data['신고일시'].dt.year,data['신고일시'].dt.month]).mean('소요시간')
+# group = group.sort_values('소요시간',ascending = False).head(1)
+# result = group['소요시간'].iloc[0]/60
+# answer = int(round(result,0))
+# print(answer)
 
-data = data[data['country']=='United Kingdom']
-
-answer=(len(data))
-
-print(answer)
