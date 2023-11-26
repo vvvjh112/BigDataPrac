98aef0c06a79a4fce60806a0979a222e1574f36d
vvvjh112 <lleell6@naver.com>
2023-11-26 17:32:35 +0900

    .

--- exam_1.py
+++ exam_1.py
@@ -147,16 +147,130 @@
 # print(result)
 
 #3-2
+# import pandas as pd
+# import numpy as np
+# from scipy.stats import *
+#
+# data = pd.read_csv('dataset/M1-6.csv')
+#
+# a = shapiro(data['Temp'])
+# print(a)
+# # print(help(ttest_1samp))
+# # result = ttest_1samp(data['Temp'],popmean=75)
+#
+# result = wilcoxon(data['Temp'])
+# print(result)
+
+
+
+#1-1
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/M1-1.csv')
+#
+# train = data.head(int(len(data)*0.7))
+#
+# result = train.sort_values('price',ascending = False).head(5)
+#
+# answer = int(train['depth'].median())
+#
+# print(answer)
+
+#61
+
+#1-2
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/M1-2.csv')
+#
+# data['TotalCharges'] = pd.to_numeric(data['TotalCharges'],errors = "coerce")
+# data = data.dropna(subset=['TotalCharges'])
+#
+# print(data.isna().sum())
+#
+# maxi = data['TotalCharges'].mean()+(data['TotalCharges'].std()*1.5)
+# mini = data['TotalCharges'].mean()-(data['TotalCharges'].std()*1.5)
+#
+# result = data[(data['TotalCharges']>mini)&(data['TotalCharges']<maxi)]
+#
+# answer = int(result['TotalCharges'].mean())
+#
+# print(answer)
+
+#1663
+
+#1-3
+# import pandas as pd
+# import numpy as np
+#
+# data = pd.read_csv('dataset/M1-3.csv')
+#
+# set1 = data[data['am']==1].sort_values('hp',ascending = True).head(5)
+# set2 = data[data['am']==0].sort_values('hp',ascending = True).head(5)
+#
+# result1 = set1['mpg'].mean()
+# result2 = set2['mpg'].mean()
+#
+# answer = round(abs(result1-result2),1)
+#
+# print(answer)
+#8.4
+
+
+#2
+
+
+
+
+#3-1
+# import pandas as pd
+# import numpy as np
+#
+# from scipy.stats import *
+#
+# data = pd.DataFrame({'before':[200,210,190,180,175],'after':[180,175,160,150,160]})
+#
+# result = ttest_rel(data['after'],data['before'],alternative = 'less')
+#
+# answer1 = round(result.statistic,5)
+# answer2 = round(result.pvalue,5)
+# if result.pvalue<0.05:
+#     answer3 = "기각"
+# else:
+#     answer3= "채택"
+# print(answer1)
+# print(answer2)
+# print(answer3)
+# -7.0763
+# 0.00105
+# 기각
+
+
+#3-2
 import pandas as pd
 import numpy as np
-from scipy.stats import *
 
 data = pd.read_csv('dataset/M1-6.csv')
-
-a = shapiro(data['Temp'])
-print(a)
-# print(help(ttest_1samp))
-# result = ttest_1samp(data['Temp'],popmean=75)
-
+from scipy.stats import *
+import scipy.stats
+# print(dir(scipy.stats))
+test = shapiro(data['Temp'])
+print(test)
 result = wilcoxon(data['Temp'])
-print(result)
+answer1 = round(result.statistic,3)
+answer2 = round(result.pvalue,3)
+
+if result.pvalue<0.05:
+    answer3 = "기각"
+else:
+    answer3= "채택"
+
+print(answer1)
+print(answer2)
+print(answer3)
+
+# 3.767
+# 0.0
+# 기각
